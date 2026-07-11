9b4d8f4c5af95f7b29c342c85538f9abe45d9c55
Jiya Adhikari <jiyaadhikarii05@gmail.com>
2026-07-11 14:33:11 +0530

    Add Insertion Sort implementation in C++

---
+++ insertion_sort.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+void insertion_sort(int arr[],int n){
+    for(int i=0;i<=n-1;i++){
+        int j=i;
+
+        while(j>0 && arr[j-1]>arr[j]){
+            int temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+
+            j--;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cout<<"enter n:";
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"enter elements:";
+        cin>>arr[i];
+    }
+    cout<<"unsorted array:"<<endl;
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+       
+    }
+
+    insertion_sort(arr,n);
+    cout<<endl;
+    cout<<"sorted array:"<<endl;
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+       
+    }
+
+
+}
